322474547404b81d84dc1a9046f7431a0587d2af
henrique.nakashima <none@none>
2009-07-18 00:46:48 -0300

    - Fixed import problems in maptest32.py

--- test/maptest32.py
+++ test/maptest32.py
@@ -14,13 +14,13 @@ mode = int(sys.argv[1])
 if mode == 1:
     pass
 elif mode == 2:
-    librpg.graphics_config.config(camera_mode=FixedCameraMode(50, 50), screen_width=480, screen_height=480, scale=1)
+    librpg.graphics_config.config(camera_mode=librpg.camera.FixedCameraMode(50, 50), screen_width=480, screen_height=480, scale=1)
 elif mode == 3:
-    librpg.graphics_config.config(camera_mode=PartyConfinementCameraMode(50, 40), screen_width=400, screen_height=300)
+    librpg.graphics_config.config(camera_mode=librpg.camera.PartyConfinementCameraMode(50, 40), screen_width=400, screen_height=300)
 elif mode == 4:
-    librpg.graphics_config.config(camera_mode=ScreenConfinementCameraMode(), screen_width=200, screen_height=200, scale=3)
+    librpg.graphics_config.config(camera_mode=librpg.camera.ScreenConfinementCameraMode(), screen_width=200, screen_height=200, scale=3)
 elif mode == 5:
-    librpg.graphics_config.config(camera_mode=ScreenConfinementCameraMode(), screen_width=400, screen_height=400)
+    librpg.graphics_config.config(camera_mode=librpg.camera.ScreenConfinementCameraMode(), screen_width=400, screen_height=400)
 else:
     print 'Pass a number from 1 to 5 for the screen and camera mode.'
     exit()
