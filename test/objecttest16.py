229de25d9bfa320777d6f9943a44a6c241580cf2
henrique.nakashima <none@none>
2009-08-07 03:46:41 -0300

    -Adding objecttest16.py for testing object drawing order

---
+++ test/objecttest16.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.append('..')
+
+import librpg
+import pygame
+
+librpg.init()
+librpg.graphics_config.config(tile_size=16, object_height=32, object_width=24)
+
+from librpg.map import MapModel, Map
+from librpg.mapobject import MapObject, ScenarioMapObject
+from librpg.util import Position
+from librpg.party import Character, CharacterReserve
+from librpg.movement import Slide, Wait, ForcedStep
+from librpg.dialog import Dialog
+from librpg.locals import *
+
+class ObjectTestNPC(MapObject):
+
+    def __init__(self, index):
+    
+        MapObject.__init__(self, MapObject.OBSTACLE, image_file='chara1.png', image_index=index)
+        self.movement_behavior.movements.extend([Wait(30), ForcedStep(UP), Wait(30), ForcedStep(DOWN)])
+
+
+class ObjectTestRock(ScenarioMapObject):
+
+    def __init__(self, map):
+    
+        ScenarioMapObject.__init__(self, map, 0, 57)
+        
+    def collide_with_party(self, party_avatar, direction):
+    
+        print 'Collided rock'
+        self.schedule_movement(Slide(direction))
+
+
+class ObjectTestMap(MapModel):
+    
+    def __init__(self):
+    
+        MapModel.__init__(self, 'objecttest16.map', ('lower_tileset.png', 'lower_tileset.bnd'), [('upper_tileset.png', 'upper_tileset.bnd'),])
+        
+    def initialize(self, local_state):
+    
+        index = 0
+        for i in range(6, 2, -1):
+            for j in range(3, 1, -1):
+                self.add_object(ObjectTestNPC(index), Position(i, j))
+                index = (index + 1) % 8
+                
+        self.add_object(ObjectTestRock(self), Position(7, 2))
+
+
+a = librpg.party.Character('Andy', 'chara1.png', 3)
+r = librpg.party.CharacterReserve([a])
+
+model = ObjectTestMap()
+model.add_party(r.create_party(3, [a]), Position(8, 8))
+Map(model).gameloop()
+exit()
