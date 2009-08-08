7a384be8738e1686f9cc0f600fa31064401f8c2b
henrique.nakashima <none@none>
2009-08-08 05:39:41 -0300

    - Victory message added to bouldertest.py

--- test/bouldertest.py
+++ test/bouldertest.py
@@ -8,10 +8,11 @@ librpg.init()
 librpg.config.graphics_config.config(tile_size=32, object_height=32, object_width=32)
 
 from librpg.map import MapModel, Map
-from librpg.mapobject import ScenarioMapObject
+from librpg.mapobject import ScenarioMapObject, MapObject
 from librpg.util import Position
 from librpg.party import Character, CharacterReserve
 from librpg.movement import Slide
+from librpg.dialog import MessageDialog
 
 class Boulder(ScenarioMapObject):
 
@@ -23,13 +24,23 @@ class Boulder(ScenarioMapObject):
     
         self.schedule_movement(Slide(direction))
 
+class Victory(ScenarioMapObject):
+
+    def __init__(self, map):
+    
+        ScenarioMapObject.__init__(self, map, 0, 0)
+        
+    def collide_with_party(self, party_avatar, direction):
+    
+        self.map.schedule_message(MessageDialog('Gratz! You won!'))
+
 
 class BoulderMaze(MapModel):
     
     MAZE = [
-    [1,1,1,1,1,1,1,1,1,1],
+    [1,1,1,1,3,3,1,1,1,1],
     [0,0,0,0,1,2,0,0,0,0],
-    [0,1,1,2,2,2,1,2,1,0],
+    [0,1,1,2,2,1,1,2,1,0],
     [0,1,2,1,1,1,2,1,2,0],
     [0,2,1,1,1,2,1,1,2,0],
     [0,1,2,2,2,1,1,2,1,0],
@@ -40,13 +51,15 @@ class BoulderMaze(MapModel):
 
     def __init__(self):
     
-        MapModel.__init__(self, 'bouldertest.map', ('lower_tileset32.png', 'lower_tileset32.bnd'), [('upper_tileset32.png', 'upper_tileset32.bnd')] )
+        MapModel.__init__(self, 'bouldertest.map', ('lower_tileset32.png', 'lower_tileset32.bnd'), [('upper_tileset32.png', 'upper_tileset32.bnd')])
         
     def initialize(self, local_state):
         for y, line in enumerate(BoulderMaze.MAZE):
             for x, cell in enumerate(line):
                 if cell == 2:
                     self.add_object(Boulder(self), Position(x, y))
+                elif cell == 3:
+                    self.add_object(Victory(self), Position(x, y))
 
 a = librpg.party.Character('Andy', 'char_alex32.png')
 r = librpg.party.CharacterReserve([a])
