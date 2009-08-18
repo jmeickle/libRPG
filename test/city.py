4491988e817793c69ac29abb21553b024dfbae2b
henrique.nakashima <none@none>
2009-08-17 22:46:10 -0300

    - City upper tileset added

--- test/city.py
+++ test/city.py
@@ -5,7 +5,7 @@ librpg.init()
 librpg.config.graphics_config.config(tile_size=32, object_height=32, object_width=32)
 librpg.config.graphics_config.config(camera_mode=librpg.camera.PartyConfinementCameraMode(50, 40), screen_width=500, screen_height=400)
 
-m = librpg.map.MapModel('city.map', ('city32.png', 'city32.bnd'), [])
+m = librpg.map.MapModel('city.map', ('city32.png', 'city32.bnd'), [('city_upper32.png', 'city_upper32.bnd')])
 
 a = librpg.party.Character('Andy', 'actor1.png', 0)
 r = librpg.party.CharacterReserve([a])
