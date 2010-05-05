f8dc7ffb0aadd192da8bbb74120146c1bf33a2b4
Henrique Nakashima <henrique.nakashima@gmail.com>
2010-05-05 01:22:10 -0300

    - Adding more usable items to itembase.py

--- librpg/test/itembase.py
+++ librpg/test/itembase.py
@@ -1,9 +1,23 @@
 from librpg.util import IdFactory
 from librpg.item import OrdinaryItem, UsableOrdinaryItem
-from librpg.dialog import ChoiceDialog
+from librpg.dialog import ChoiceDialog, MessageDialog
 
 
 item_factory = IdFactory()
+
+
+class SinglePartyTargetItem(UsableOrdinaryItem):
+    
+    def use(self, party):
+        dialog = ChoiceDialog('Use on whom?', party.chars)
+        dialog.sync_open()
+        
+        target = party.chars[dialog.result]
+        self.use_on_party_member(party.get_char(target))
+        
+    def use_on_party_member(self, character):
+        raise NotImplementedError('SinglePartyTargetItem.\
+                                   use_on_party_member() is abstract')
 
 
 class LogItem(OrdinaryItem):
@@ -24,38 +38,48 @@ class LeafItem(OrdinaryItem):
 item_factory.register(LeafItem)
 
 
-class PotionItem(UsableOrdinaryItem):
+class PotionItem(SinglePartyTargetItem):
 
     id = 'potion'
 
     def __init__(self):
-        UsableOrdinaryItem.__init__(self, 'Potion')
+        SinglePartyTargetItem.__init__(self, 'Potion')
 
-    def use(self, party):
-        dialog = ChoiceDialog('Use on whom?', party.chars)
+    def use_on_party_member(self, character):
+        msg = 'Using %s on %s' % (self.name, character)
+        dialog = MessageDialog(msg, True)
         dialog.sync_open()
-        
-        target = party.chars[dialog.result]
-        print 'Using on %s' % (target)
-    
+
 item_factory.register(PotionItem)
 
 
-class ElixirItem(OrdinaryItem):
+class ElixirItem(SinglePartyTargetItem):
 
     id = 'elixir'
 
     def __init__(self):
-        OrdinaryItem.__init__(self, 'Elixir')
+        SinglePartyTargetItem.__init__(self, 'Elixir')
+        
+    def use_on_party_member(self, character):
+        msg = 'Using %s on %s' % (self.name, character)
+        dialog = MessageDialog(msg, True)
+        dialog.sync_open()
+
 item_factory.register(ElixirItem)
 
 
-class FeatherItem(OrdinaryItem):
+class FeatherItem(SinglePartyTargetItem):
 
     id = 'feather'
 
     def __init__(self):
-        OrdinaryItem.__init__(self, 'Feather')
+        SinglePartyTargetItem.__init__(self, 'Feather')
+        
+    def use_on_party_member(self, character):
+        msg = 'Using %s on %s' % (self.name, character)
+        dialog = MessageDialog(msg, True)
+        dialog.sync_open()
+
 item_factory.register(FeatherItem)
 
 
@@ -68,30 +92,48 @@ class EmptyVialItem(OrdinaryItem):
 item_factory.register(EmptyVialItem)
 
 
-class LavenderItem(OrdinaryItem):
+class LavenderItem(SinglePartyTargetItem):
 
     id = 'lavender'
 
     def __init__(self):
-        OrdinaryItem.__init__(self, 'Lavender')
+        SinglePartyTargetItem.__init__(self, 'Lavender')
+        
+    def use_on_party_member(self, character):
+        msg = 'Using %s on %s' % (self.name, character)
+        dialog = MessageDialog(msg, True)
+        dialog.sync_open()
+
 item_factory.register(LavenderItem)
 
 
-class BasilItem(OrdinaryItem):
+class BasilItem(SinglePartyTargetItem):
 
     id = 'basil'
 
     def __init__(self):
-        OrdinaryItem.__init__(self, 'Basil')
+        SinglePartyTargetItem.__init__(self, 'Basil')
+        
+    def use_on_party_member(self, character):
+        msg = 'Using %s on %s' % (self.name, character)
+        dialog = MessageDialog(msg, True)
+        dialog.sync_open()
+
 item_factory.register(BasilItem)
 
 
-class RosemaryItem(OrdinaryItem):
+class RosemaryItem(SinglePartyTargetItem):
 
     id = 'rosemary'
 
     def __init__(self):
-        OrdinaryItem.__init__(self, 'Rosemary')
+        SinglePartyTargetItem.__init__(self, 'Rosemary')
+        
+    def use_on_party_member(self, character):
+        msg = 'Using %s on %s' % (self.name, character)
+        dialog = MessageDialog(msg, True)
+        dialog.sync_open()
+
 item_factory.register(RosemaryItem)
 
 
